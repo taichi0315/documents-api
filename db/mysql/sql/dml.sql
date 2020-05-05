d4992662b3cf4db0c8eb64021513103d53aed8c0
taichi0315 <k.taichi0315@gmail.com>
2020-05-05 21:34:38 +0900

    fixed password on dml

--- db/mysql/sql/dml.sql
+++ db/mysql/sql/dml.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(username) VALUES ("yaga");
 
-INSERT INTO user_auths(user_id,email,hash) VALUES (1,"yaga@example.com","password");
+INSERT INTO user_auths(user_id,email,hash) VALUES (1,"yaga@example.com","$pbkdf2-sha512$531$AJGxFyMsRFPDT0p6KiME8B.vPbUg62PziTeIipt2twQ$njA3JX4OrF2OnCIZkr7mn.Tn4FAQkk1a/eJuMzYXDm4");
 
 INSERT INTO user_details(user_id,nickname) VALUES (1,"たいち");
 
