de93f2c112bd3d4de41b009562060735d4e4da7c
taichi0315 <k.taichi0315@gmail.com>
2020-05-11 22:44:38 +0900

    fixed db name

--- app/lib/persistence/db/UserDetail.scala
+++ app/lib/persistence/db/UserDetail.scala
@@ -15,8 +15,8 @@ case class UserDetailTable[P <: JdbcProfile]()(implicit val driver: P)
   // Definition of DataSourceName
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   lazy val dsn = Map(
-    "master" -> DataSourceName("ixias.db.mysql://master/auth_api"),
-    "slave"  -> DataSourceName("ixias.db.mysql://slave/auth_api")
+    "master" -> DataSourceName("ixias.db.mysql://master/documents_api"),
+    "slave"  -> DataSourceName("ixias.db.mysql://slave/documents_api")
   )
 
   // Definition of Query
