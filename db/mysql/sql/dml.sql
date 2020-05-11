93d13abbade133cd508080cad9bd1182df8aec9b
taichi0315 <k.taichi0315@gmail.com>
2020-05-11 22:56:16 +0900

    add dml

--- db/mysql/sql/dml.sql
+++ db/mysql/sql/dml.sql
@@ -5,3 +5,8 @@ INSERT INTO user_auths(user_id,email,hash) VALUES (1,"yaga@example.com","$pbkdf2
 INSERT INTO user_details(user_id,nickname) VALUES (1,"たいち");
 
 INSERT INTO auth_tokens(user_id,token,expiry) VALUES (1,"token","2020-05-02 21:00:00");
+
+INSERT INTO documents(url,user_id) VALUES
+("https://go-tour-jp.appspot.com/welcome/1",1),
+("https://soudai.hatenablog.com/entry/2018/05/01/204442",1),
+("https://angular.jp/tutorial",1);
